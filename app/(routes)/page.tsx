f86add43668915c0395e02ed877aa09baed1b8b7
Andrei <andryushik@gmail.com>
2023-09-05 17:04:05 +0200

    billboard added v8.00

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const HomePage = () => {
-  return <div>hello store</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0; // for no-cashing
+
+const HomePage = async () => {
+  const billboard = await getBillboard('6d93daee-2322-4652-b147-8502f657c363');
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
+s;
 
 export default HomePage;
